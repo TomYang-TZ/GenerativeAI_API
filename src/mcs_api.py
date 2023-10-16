ad32f538a6d0b7b2edbfedea60ad2d1718916ecd
Tom Yang <tomyangtianze@gmail.com>
2023-10-16 16:30:20 -0400

    adjusted functions to meet the needs of UI

--- src/mcs_api.py
+++ src/mcs_api.py
@@ -27,7 +27,7 @@ access_token = os.environ["ACCESS_TOKEN"]
 
 def get_image(url='https://pgmwn8b5xu.meta.crosschain.computer',
               username='admin',password='admin1234',
-              prompt='a green apple',negative_prompt='red',
+              prompt='a green apple',negative_prompt='violent',
               sampler_name='DPM++ 2M Karras',
               seed=985454925,cfg_scale=7,steps=20,width=512,height=512):
     # Encode the username and password in Base64
@@ -54,37 +54,39 @@ def get_image(url='https://pgmwn8b5xu.meta.crosschain.computer',
     image = Image.open(io.BytesIO(base64.b64decode(r['images'][0])))
     return image
 
-def initialize_client(chain="polygon.mumbai"):
+def initialize_client(chain="polygon.mumbai",api_key=api_key,access_token=access_token):
     mcs_api = APIClient(api_key, chain, access_token)
     return BucketAPI(mcs_api)
 
-def save_to_MCS_bucket(bucket_client, bucket_name="diffusion2",file_path="output.png",
+def save_to_MCS_bucket(bucket_client, bucket_name="diffusion2",file_path="output.png",name="output.png",
                        overwrite_file=False,initialize_bucket=True):
     if initialize_bucket: bucket_client.create_bucket(bucket_name)   
     if overwrite_file:
-        print(f"Overwriting file {file_path}")
-        bucket_client.delete_file(bucket_name, file_path)
-    file_data = bucket_client.upload_file(bucket_name, file_path , file_path) 
+        print(f"Overwriting file {name}")
+        bucket_client.delete_file(bucket_name, name)
+    file_data = bucket_client.upload_file(bucket_name, name , file_path) 
     if not file_data:
         print("upload failed")
         return 
     file_data = json.loads(file_data.to_json())
+    print(file_data)
+    return file_data["ipfs_url"]  
     
-    bucket_link = f"https://multichain.storage/my_buckets_detail?folder={bucket_name}&bucket_uuid={file_data['bucket_uid']}"
-    return file_data["ipfs_url"],bucket_link  
-    
+def pipeline(prompt, access_token, api_key, folder_name, image_name, seed,initialize_bucket=True,overwrite_file=False,url='https://pgmwn8b5xu.meta.crosschain.computer'):
+    img = get_image(prompt=prompt,url=url,seed=seed)
+    img.save("output.png")  
+    bucket_client = initialize_client(api_key=api_key,access_token=access_token)
 
-    
+    img_link = save_to_MCS_bucket(bucket_client,initialize_bucket=initialize_bucket,overwrite_file=overwrite_file,
+                                  bucket_name=folder_name,name=image_name)
 
+    return img_link
 
 if __name__ == '__main__':
     bucket_client = initialize_client()
     img = get_image(prompt="a blue apple")
     img.save("output.png")
-    try:
-        img_link, bucket_link = save_to_MCS_bucket(bucket_client,initialize_bucket=True,overwrite_file=False)
-    except:
-        img_link,bucket_link = None,None
+    img_link = save_to_MCS_bucket(bucket_client,initialize_bucket=True,overwrite_file=True)
     print("Image Link: ", img_link)
-    print("Bucket Link: ", bucket_link)
+    
     

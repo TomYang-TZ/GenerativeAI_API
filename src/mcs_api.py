bf2a8d99f0edf99b054ee9ee52cfc1f32f10e323
Tom Yang <tomyangtianze@gmail.com>
2023-10-18 16:44:15 -0400

    file update

--- src/mcs_api.py
+++ src/mcs_api.py
@@ -67,18 +67,19 @@ def save_to_MCS_bucket(bucket_client, bucket_name="diffusion2",file_path="output
     file_data = bucket_client.upload_file(bucket_name, name , file_path) 
     if not file_data:
         print("upload failed")
-        return 
+        raise Exception("upload failed") 
     file_data = json.loads(file_data.to_json())
     print(file_data)
     return file_data["ipfs_url"]  
     
-def pipeline(prompt, access_token, api_key, folder_name, image_name, seed,initialize_bucket=True,overwrite_file=False,url='https://pgmwn8b5xu.meta.crosschain.computer'):
+def pipeline(prompt, access_token, api_key, folder_name, image_name, seed,
+             initialize_bucket=True,overwrite_file=False,url='https://pgmwn8b5xu.meta.crosschain.computer',file_path="output.png"):
     img = get_image(prompt=prompt,url=url,seed=seed)
-    img.save("output.png")  
+    img.save(file_path)  
     bucket_client = initialize_client(api_key=api_key,access_token=access_token)
 
     img_link = save_to_MCS_bucket(bucket_client,initialize_bucket=initialize_bucket,overwrite_file=overwrite_file,
-                                  bucket_name=folder_name,name=image_name)
+                                  bucket_name=folder_name,file_path=file_path,name=image_name)
 
     return img_link
 
